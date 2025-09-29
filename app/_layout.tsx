d38acf092cde12ebcdd8386e4074a8c89eb78270
Saurabh <risabht043@gmail.com>
2025-09-29 16:31:12 +0530

    fix issues

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import "./global.css"
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
 import * as SplashScreen from "expo-splash-screen";
+import GlobalProvider from "@/lib/global-provider";
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
@@ -23,5 +24,9 @@ export default function RootLayout() {
   if (!fontsLoaded) {
     return null;
   }
-  return <Stack  screenOptions={{headerShown: false}}/>;
+  return (
+    <GlobalProvider>
+      <Stack screenOptions={{ headerShown: false }} />
+    </GlobalProvider>
+  )
 }
